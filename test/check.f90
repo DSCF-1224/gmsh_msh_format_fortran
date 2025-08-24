16ecb5c0aa8e26556dbd6485a98a149670f13c25
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-08-24 18:35:13 +0900

    Merge branch 'develop_test' into develop
    - docs: update README
    - fix: how to detect reading header/footer
    - fix: how to handle iostat
    - test: add test for the MSH files (ASCII)
    - ci: add a job to test this fortran module
    - test: add a program to test this fortran module

---
+++ test/check.f90
@@ -0,0 +1,101 @@
+program check
+
+    use, intrinsic :: iso_fortran_env , only: &!
+        error_unit       , &!
+        output_unit      , &!
+        compiler_options , &!
+        compiler_version
+
+    use, non_intrinsic :: gmsh_msh_format_fortran
+
+
+
+    implicit none
+
+
+
+    print *, compiler_version()
+    print *, compiler_options()
+
+    call test_formatted( 'test/empty-ASCII.msh2' )
+    call test_formatted( 'test/empty-ASCII.msh4' )
+
+
+
+    contains
+
+
+
+    subroutine test_formatted(file)
+
+        character(*), intent(in) :: file
+
+
+
+        integer :: unit, iostat
+
+        character(256) :: iomsg
+
+        type(gmsh_msh_format_type) :: msh_format
+
+
+
+        open( &!
+        newunit = unit        , &!
+        file    = file        , &!
+        form    = 'formatted' , &!
+        iostat  = iostat      , &!
+        iomsg   = iomsg         &!
+        )
+
+        call handle_io_error(file = file, iostat = iostat, iomsg = iomsg )
+
+
+
+        read( &!
+        unit   = unit   , &!
+        fmt    = *      , &!
+        iostat = iostat , &!
+        iomsg  = iomsg    &!
+        ) &!
+        msh_format
+
+        call handle_io_error(file = file, iostat = iostat, iomsg = iomsg )
+
+
+
+        close( &!
+        unit   = unit   , &!
+        iostat = iostat , &!
+        iomsg  = iomsg    &!
+        )
+
+        call handle_io_error(file = file, iostat = iostat, iomsg = iomsg )
+
+    end subroutine test_formatted
+
+
+
+    subroutine handle_io_error(file, iostat, iomsg)
+
+        character(*), intent(in) :: file
+
+        integer, intent(in) :: iostat
+
+        character(*), intent(in) :: iomsg
+
+
+
+        if ( iostat .ne. 0 ) then
+
+            write( error_unit, '(A,1X,A)'  ) 'file   :', trim(file)
+            write( error_unit, '(A,1X,I0)' ) 'iostat :', iostat
+            write( error_unit, '(A,1X,A)'  ) 'iomsg  :', trim(iomsg)
+
+            error stop
+
+        end if
+
+    end subroutine handle_io_error
+
+end program check
