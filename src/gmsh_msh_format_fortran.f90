756a7082d26bb8dd7b1a168c48e95de222016e4e
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-08-26 06:50:39 +0900

    feat: add implementation: read(unformatted)

--- src/gmsh_msh_format_fortran.f90
+++ src/gmsh_msh_format_fortran.f90
@@ -2,7 +2,7 @@
 !> Implementation of a derived type to hold `$MeshFormat` of Gmsh MSH file format.
 module gmsh_msh_format_fortran
 
-    use, intrinsic :: iso_c_binding, only: c_int, c_double
+    use, intrinsic :: iso_c_binding, only: c_int, c_double, c_new_line
 
 
 
@@ -17,6 +17,7 @@ module gmsh_msh_format_fortran
     public :: gmsh_msh_format_type
 
     public :: read(formatted)
+    public :: read(unformatted)
 
     public :: export_data_size
     public :: export_file_type
@@ -58,6 +59,14 @@ module gmsh_msh_format_fortran
     interface read(formatted)
         module procedure :: read_formatted
     end interface read(formatted)
+
+
+
+    !> Version: experimental
+    !> Read an `$MshMeshFormat` from a connected unformatted unit.
+    interface read(unformatted)
+        module procedure :: read_unformatted
+    end interface read(unformatted)
 
 
 
@@ -258,6 +267,213 @@ module gmsh_msh_format_fortran
         )
 
     end subroutine read_formatted_kernel
+
+
+
+    !> Version: experimental
+    !> Read an `$MshMeshFormat` from a connected unformatted unit.
+    subroutine read_unformatted(gmsh_msh_format, unit, iostat, iomsg)
+
+        class(gmsh_msh_format_type), intent(inout) :: gmsh_msh_format
+
+        integer, intent(in) :: unit
+
+        integer, intent(out) :: iostat
+
+        character(*), intent(inout) :: iomsg
+
+
+
+        integer :: itr
+
+        !> Version: experimental
+        !> A `character` variable to read a character.
+        character(1) :: buffer
+
+        !> Version: experimental
+        !> A `character` variable for holding read string.<br>
+        !> The length of this string is provisional.
+        character(32) :: str
+
+
+
+        read_header: &!
+        block
+
+            itr   = 1
+            iomsg = ''
+            str   = ''
+
+            do
+
+                read( &!
+                unit   = unit   , &!
+                iostat = iostat , &!
+                iomsg  = iomsg    &!
+                ) &!
+                buffer
+
+                if ( iostat .ne. iostat_success ) return
+
+                if ( buffer(1:1) .eq. '$' ) then
+
+                    str(itr:itr) = buffer(1:1)
+
+                    itr = itr + 1
+
+                    exit
+
+                end if
+
+            end do
+
+            do
+
+                read( &!
+                unit   = unit   , &!
+                iostat = iostat , &!
+                iomsg  = iomsg    &!
+                ) &!
+                buffer
+
+                if ( iostat .ne. iostat_success ) return
+
+                if ( buffer(1:1) .eq. c_new_line ) then
+
+                    call check_text_line( &!
+                    text_line = str(1:itr) , &!
+                    str       = header     , &!
+                    iostat    = iostat     , &!
+                    iomsg     = iomsg        &!
+                    )
+
+                    exit
+
+                end if
+
+                str(itr:itr) = buffer(1:1)
+
+                itr = itr + 1
+
+            end do
+
+            if ( iostat .ne. iostat_success ) return
+
+        end block &!
+        read_header
+
+
+
+        read_values: &!
+        block
+
+            itr = 1
+            str = ''
+
+            do
+
+                read( &!
+                unit   = unit   , &!
+                iostat = iostat , &!
+                iomsg  = iomsg    &!
+                ) &!
+                buffer
+
+                if ( iostat .ne. iostat_success ) return
+
+                if ( buffer(1:1) .eq. c_new_line ) exit
+
+                str(itr:itr) = buffer(1:1)
+
+                itr = itr + 1
+
+            end do
+
+            read( &!
+            unit   = str(1:itr) , &!
+            fmt    = *          , &!
+            iostat = iostat     , &!
+            iomsg  = iomsg        &!
+            ) &!
+            gmsh_msh_format%version   , &!
+            gmsh_msh_format%file_type , &!
+            gmsh_msh_format%data_size
+
+            if ( iostat .ne. iostat_success ) return
+
+        end block &!
+        read_values
+
+
+
+        read_footer: &!
+        block
+
+            itr   = 1
+            iomsg = ''
+            str   = ''
+
+            do
+
+                read( &!
+                unit   = unit   , &!
+                iostat = iostat , &!
+                iomsg  = iomsg    &!
+                ) &!
+                buffer
+
+                if ( iostat .ne. iostat_success ) return
+
+                if ( buffer(1:1) .eq. '$' ) then
+
+                    str(itr:itr) = buffer(1:1)
+
+                    itr = itr + 1
+
+                    exit
+
+                end if
+
+            end do
+
+            do
+
+                read( &!
+                unit   = unit   , &!
+                iostat = iostat , &!
+                iomsg  = iomsg    &!
+                ) &!
+                buffer
+
+                if ( iostat .ne. iostat_success ) return
+
+                if ( buffer(1:1) .eq. c_new_line ) then
+
+                    call check_text_line( &!
+                    text_line = str(1:itr) , &!
+                    str       = footer     , &!
+                    iostat    = iostat     , &!
+                    iomsg     = iomsg        &!
+                    )
+
+                    exit
+
+                end if
+
+                str(itr:itr) = buffer(1:1)
+
+                itr = itr + 1
+
+            end do
+
+            print *, str(1:itr)
+
+            if ( iostat .ne. iostat_success ) return
+
+        end block &!
+        read_footer
+
+    end subroutine read_unformatted
 
 
 
