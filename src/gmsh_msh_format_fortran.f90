f7e0127856dce539dfc82de57667ce634861acdd
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-08-31 21:44:56 +0900

    Merge branch 'develop' into main
    - refactor: fix the comment: export_version interface
    - test: add ascii/binary mode check
    - feat: add is_ascii/binary
    - test: add test for the MSH files (binary)
    - refactor!: remove unnecessary print statement
    - feat: add implementation: read(unformatted)
    - test: add msh2/msh4 file to test this fortran module

--- src/gmsh_msh_format_fortran.f90
+++ src/gmsh_msh_format_fortran.f90
@@ -2,7 +2,7 @@
 !> Implementation of a derived type to hold `$MeshFormat` of Gmsh MSH file format.
 module gmsh_msh_format_fortran
 
-    use, intrinsic :: iso_c_binding, only: c_int, c_double
+    use, intrinsic :: iso_c_binding, only: c_int, c_double, c_new_line
 
 
 
@@ -17,10 +17,13 @@ module gmsh_msh_format_fortran
     public :: gmsh_msh_format_type
 
     public :: read(formatted)
+    public :: read(unformatted)
 
     public :: export_data_size
     public :: export_file_type
     public :: export_version
+    public :: is_ascii
+    public :: is_binary
 
 
 
@@ -62,6 +65,14 @@ module gmsh_msh_format_fortran
 
 
     !> Version: experimental
+    !> Read an `$MshMeshFormat` from a connected unformatted unit.
+    interface read(unformatted)
+        module procedure :: read_unformatted
+    end interface read(unformatted)
+
+
+
+    !> Version: experimental
     !> Exports the `data-size` of the read Gmsh MSH file format as a `c_int` value.
     interface export_data_size
         module procedure :: export_data_size_from_format
@@ -77,12 +88,27 @@ module gmsh_msh_format_fortran
 
 
 
-    !> Exports the file
     !> Version: experimental
     !> Exports the version of the read Gmsh MSH file format as a `c_double` value.
     interface export_version
         module procedure :: export_version_from_format
     end interface export_version
+
+
+
+    !> Version: experimental
+    !> Checks if the read Gmsh MSH file is ASCII mode
+    interface is_ascii
+        module procedure :: is_ascii_gmsh_msh_format
+    end interface is_ascii
+
+
+
+    !> Version: experimental
+    !> Checks if the read Gmsh MSH file is binary mode
+    interface is_binary
+        module procedure :: is_binary_gmsh_msh_format
+    end interface is_binary
 
 
 
@@ -135,6 +161,38 @@ module gmsh_msh_format_fortran
         version = gmsh_msh_format%version
 
     end function export_version_from_format
+
+
+
+    !> Version: experimental
+    !> Checks if the read Gmsh MSH file is ASCII mode
+    elemental function is_ascii_gmsh_msh_format(gmsh_msh_format) result(res)
+
+        type(gmsh_msh_format_type), intent(in) :: gmsh_msh_format
+
+        logical :: res
+
+
+
+        res = gmsh_msh_format%file_type .eq. 0_c_int
+
+    end function is_ascii_gmsh_msh_format
+
+
+
+    !> Version: experimental
+    !> Checks if the read Gmsh MSH file is binary mode
+    elemental function is_binary_gmsh_msh_format(gmsh_msh_format) result(res)
+
+        type(gmsh_msh_format_type), intent(in) :: gmsh_msh_format
+
+        logical :: res
+
+
+
+        res = gmsh_msh_format%file_type .eq. 1_c_int
+
+    end function is_binary_gmsh_msh_format
 
 
 
@@ -258,6 +316,211 @@ module gmsh_msh_format_fortran
         )
 
     end subroutine read_formatted_kernel
+
+
+
+    !> Version: experimental
+    !> Read an `$MshMeshFormat` from a connected unformatted unit.
+    subroutine read_unformatted(gmsh_msh_format, unit, iostat, iomsg)
+
+        class(gmsh_msh_format_type), intent(inout) :: gmsh_msh_format
+
+        integer, intent(in) :: unit
+
+        integer, intent(out) :: iostat
+
+        character(*), intent(inout) :: iomsg
+
+
+
+        integer :: itr
+
+        !> Version: experimental
+        !> A `character` variable to read a character.
+        character(1) :: buffer
+
+        !> Version: experimental
+        !> A `character` variable for holding read string.<br>
+        !> The length of this string is provisional.
+        character(32) :: str
+
+
+
+        read_header: &!
+        block
+
+            itr   = 1
+            iomsg = ''
+            str   = ''
+
+            do
+
+                read( &!
+                unit   = unit   , &!
+                iostat = iostat , &!
+                iomsg  = iomsg    &!
+                ) &!
+                buffer
+
+                if ( iostat .ne. iostat_success ) return
+
+                if ( buffer(1:1) .eq. '$' ) then
+
+                    str(itr:itr) = buffer(1:1)
+
+                    itr = itr + 1
+
+                    exit
+
+                end if
+
+            end do
+
+            do
+
+                read( &!
+                unit   = unit   , &!
+                iostat = iostat , &!
+                iomsg  = iomsg    &!
+                ) &!
+                buffer
+
+                if ( iostat .ne. iostat_success ) return
+
+                if ( buffer(1:1) .eq. c_new_line ) then
+
+                    call check_text_line( &!
+                    text_line = str(1:itr) , &!
+                    str       = header     , &!
+                    iostat    = iostat     , &!
+                    iomsg     = iomsg        &!
+                    )
+
+                    exit
+
+                end if
+
+                str(itr:itr) = buffer(1:1)
+
+                itr = itr + 1
+
+            end do
+
+            if ( iostat .ne. iostat_success ) return
+
+        end block &!
+        read_header
+
+
+
+        read_values: &!
+        block
+
+            itr = 1
+            str = ''
+
+            do
+
+                read( &!
+                unit   = unit   , &!
+                iostat = iostat , &!
+                iomsg  = iomsg    &!
+                ) &!
+                buffer
+
+                if ( iostat .ne. iostat_success ) return
+
+                if ( buffer(1:1) .eq. c_new_line ) exit
+
+                str(itr:itr) = buffer(1:1)
+
+                itr = itr + 1
+
+            end do
+
+            read( &!
+            unit   = str(1:itr) , &!
+            fmt    = *          , &!
+            iostat = iostat     , &!
+            iomsg  = iomsg        &!
+            ) &!
+            gmsh_msh_format%version   , &!
+            gmsh_msh_format%file_type , &!
+            gmsh_msh_format%data_size
+
+            if ( iostat .ne. iostat_success ) return
+
+        end block &!
+        read_values
+
+
+
+        read_footer: &!
+        block
+
+            itr   = 1
+            iomsg = ''
+            str   = ''
+
+            do
+
+                read( &!
+                unit   = unit   , &!
+                iostat = iostat , &!
+                iomsg  = iomsg    &!
+                ) &!
+                buffer
+
+                if ( iostat .ne. iostat_success ) return
+
+                if ( buffer(1:1) .eq. '$' ) then
+
+                    str(itr:itr) = buffer(1:1)
+
+                    itr = itr + 1
+
+                    exit
+
+                end if
+
+            end do
+
+            do
+
+                read( &!
+                unit   = unit   , &!
+                iostat = iostat , &!
+                iomsg  = iomsg    &!
+                ) &!
+                buffer
+
+                if ( iostat .ne. iostat_success ) return
+
+                if ( buffer(1:1) .eq. c_new_line ) then
+
+                    call check_text_line( &!
+                    text_line = str(1:itr) , &!
+                    str       = footer     , &!
+                    iostat    = iostat     , &!
+                    iomsg     = iomsg        &!
+                    )
+
+                    exit
+
+                end if
+
+                str(itr:itr) = buffer(1:1)
+
+                itr = itr + 1
+
+            end do
+
+            if ( iostat .ne. iostat_success ) return
+
+        end block &!
+        read_footer
+
+    end subroutine read_unformatted
 
 
 
--- test/check.f90
+++ test/check.f90
@@ -20,6 +20,9 @@ program check
     call test_formatted( 'test/empty-ASCII.msh2' )
     call test_formatted( 'test/empty-ASCII.msh4' )
 
+    call test_unformatted( 'test/empty-binary.msh2' )
+    call test_unformatted( 'test/empty-binary.msh4' )
+
 
 
     contains
@@ -72,7 +75,77 @@ program check
 
         call handle_io_error(file = file, iostat = iostat, iomsg = iomsg )
 
+
+
+        if ( is_binary(msh_format) ) then
+
+            write( error_unit, '(A)' ) 'binary mode is detected in read(formatted)'
+
+            error stop
+
+        end if
+
     end subroutine test_formatted
+
+
+
+    subroutine test_unformatted(file)
+
+        character(*), intent(in) :: file
+
+
+
+        integer :: unit, iostat
+
+        character(256) :: iomsg
+
+        type(gmsh_msh_format_type) :: msh_format
+
+
+
+        open( &!
+        newunit = unit          , &!
+        file    = file          , &!
+        access  = 'stream'      , &!
+        form    = 'unformatted' , &!
+        iostat  = iostat        , &!
+        iomsg   = iomsg           &!
+        )
+
+        call handle_io_error(file = file, iostat = iostat, iomsg = iomsg )
+
+
+
+        read( &!
+        unit   = unit   , &!
+        iostat = iostat , &!
+        iomsg  = iomsg    &!
+        ) &!
+        msh_format
+
+        call handle_io_error(file = file, iostat = iostat, iomsg = iomsg )
+
+
+
+        close( &!
+        unit   = unit   , &!
+        iostat = iostat , &!
+        iomsg  = iomsg    &!
+        )
+
+        call handle_io_error(file = file, iostat = iostat, iomsg = iomsg )
+
+
+
+        if ( is_ascii(msh_format) ) then
+
+            write( error_unit, '(A)' ) 'ASCII mode is detected in read(unformatted)'
+
+            error stop
+
+        end if
+
+    end subroutine test_unformatted
 
 
 
